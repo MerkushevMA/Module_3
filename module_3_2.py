dd12d8c8fe3b417695bb64d13169aee294a39b1b
MerkushevMA <s2dent59@gmail.com>
2024-07-04 15:28:42 +0500

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,19 +1,20 @@
 def send_email(message, recipient, sender = 'university.help@gmail.com'):
-    list_end = ('.com', '.ru', '.net')
-
-    if recipient.find("@") == -1 or sender.find("@") == -1:
-        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
-    elif not recipient.lower().endswith(list_end) and not sender.lower().endswith(list_end):
-        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
-    elif sender == recipient:
-        print('Нельзя отправить письмо самому себе!')
-    elif sender == 'university.help@gmail.com':
-        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+    if '@' in recipient and recipient[-4:] == '.com' or recipient[-4:] == '.net' or recipient[-3:] == '.ru':
+        if '@' in sender and sender[-4:] == '.com' or sender[-4:] == '.net' or sender[-3:] == '.ru':
+            if sender == recipient:
+                print('Нельзя отправить письмо самому себе!')
+            elif sender == 'university.help@gmail.com':
+                print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+            elif sender != 'university.help@gmail.com':
+                print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+        else:
+            print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
     else:
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
 
 
-send_email('Привет растение', 'www.muxa@mail.net', 'muxa@gmail.nefjisd')
+
+send_email('Привет растение', 'www.muxamail.net')
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', 'urban.info@gmail.com')
